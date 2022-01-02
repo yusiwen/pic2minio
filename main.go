f0db53281f6d23b8c25ad20af0792b94271a9278
Siwen Yu <yusiwen@gmail.com>
2022-01-02 20:31:14 +0800

    support upload file from url

--- main.go
+++ main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"context"
+	"crypto/sha1"
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
+
+// IsValidUrl tests a string to determine if it is a well-structured url or not.
+func IsValidUrl(toTest string) bool {
+	_, err := url.ParseRequestURI(toTest)
+	if err != nil {
+		return false
+	}
+
+	u, err := url.Parse(toTest)
+	if err != nil || u.Scheme == "" || u.Host == "" {
+		return false
+	}
+
+	return true
+}
 
 func main() {
 	ctx := context.Background()
@@ -33,15 +51,37 @@ func main() {
 	}
 
 	argsWithoutProg := os.Args[1:]
-	output := []string{}
+	var output []string
 	for _, file := range argsWithoutProg {
-		base := filepath.Base(file)
-		if _, err :=
-			minioClient.FPutObject(ctx, bucket, baseDir+base,
-				file, minio.PutObjectOptions{}); err != nil {
-			log.Fatalln(err)
+		if IsValidUrl(file) {
+			h := sha1.New()
+			h.Write([]byte(file))
+			base := fmt.Sprintf("%x", h.Sum(nil)) + ".png"
+
+			func() {
+				//Get the response bytes from the url
+				response, err := http.Get(file)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				defer response.Body.Close()
+
+				if _, err := minioClient.PutObject(ctx, bucket, baseDir+"/"+base,
+					response.Body, response.ContentLength, minio.PutObjectOptions{}); err != nil {
+					log.Fatalln(err)
+				}
+				output = append(output, fmt.Sprintf("https://%s/%s/%s/%s", endpoint, bucket, baseDir, base))
+			}()
+
+		} else {
+			base := filepath.Base(file)
+			if _, err :=
+				minioClient.FPutObject(ctx, bucket, baseDir+"/"+base,
+					file, minio.PutObjectOptions{}); err != nil {
+				log.Fatalln(err)
+			}
+			output = append(output, fmt.Sprintf("https://%s/%s/%s/%s", endpoint, bucket, baseDir, base))
 		}
-		output = append(output, fmt.Sprintf("https://%s/%s/%s/%s", endpoint, bucket, baseDir, base))
 	}
 
 	for _, s := range output {
